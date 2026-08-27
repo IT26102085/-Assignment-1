fad35319cb06c83a947a295c6fe23d4982a5fe4a
IT26102085 <IT26102085@my.sliit.lk>
2026-08-27 10:46:00 +0530

    Mod initial project structure

--- source/maain.c
+++
@@ -1,9 +0,0 @@
-//This is the initial development area of the project
-
-#include<stdio.h>
-int main(){
-	
-	printf("Advanced Battleship simulator...!");
-	return 0;
-}
-
---
+++ source/main.c
@@ -0,0 +1,8 @@
+//This is the inial main source code of the programme
+
+#include<stdio.h>
+int main(){
+
+	printf("Battleship Simulator...!\n");
+	return 0;
+}
